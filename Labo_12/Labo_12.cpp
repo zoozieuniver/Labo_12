4823cb6f7d14090f6d43ceca5f1923c42519c1c4
zoozie <topjada2@gmail.com>
2025-11-29 10:09:12 +0200

    visualisation and fixing some details

--- Labo_12/Labo_12.cpp
+++ Labo_12/Labo_12.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 float power(float x, int n) { //function for recurtion
+    
+	//just for visualisation of the process
+	printf("x=%0.2f, n=%d \n", x, n);
+    
     /* Base situation:
     if n = 0, return 1 (mathematically correct).
     if n < 0, also return 1 just for comfort */
@@ -10,6 +14,7 @@ float power(float x, int n) { //function for recurtion
     }
 
     /* recurtion cycle */
+    //recurtion step
     return x * power(x, n - 1);
 }
 
@@ -25,6 +30,10 @@ int main() {
 
 		// we call the function to remember entered values
         float result = power(x, n);
+        if (n<=0){
+            printf("We can't use n<=0");
+            return 0;
+		}
 
         // print the result
         printf("Result = %.2f\n", result);
