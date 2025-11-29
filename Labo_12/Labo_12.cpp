56268fd1613a9d4643542a757f36731914c6d46b
zoozie <topjada2@gmail.com>
2025-11-29 09:45:16 +0200

    created recurtion cycle

--- Labo_12/Labo_12.cpp
+++ Labo_12/Labo_12.cpp
@@ -3,8 +3,15 @@
 #include <math.h>
 
 float power(float x, int n) { //function for recurtion
+    /* Base situation:
+    if n = 0, return 1 (mathematically correct).
+    if n < 0, also return 1 just for comfort */
+    if (n <= 0) {
+        return 1;
+    }
 
-	return x;
+    /* recurtion cycle */
+    return x * power(x, n - 1);
 }
 
 
